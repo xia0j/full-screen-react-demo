93b92124c1731d229a6f890e18588207a238e417
zhujun <zhujun@wuxicloud.com>
2025-07-14 15:45:58 +0800

    fix :初始化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
-import yayJpg from "../assets/yay.jpg";
 import Full from "@/pages/full";
 import { FullScreen } from "screen-size-react";
 import styles from "./index.less";
-import { useRef } from "react";
 import { queryMenu } from "@/apis";
 export default function HomePage() {
-  const fullScreenRef = useRef(null);
   console.log(process.env);
   queryMenu().then((res) => {});
   return (
